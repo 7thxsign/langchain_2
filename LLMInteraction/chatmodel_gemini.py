97ac01f7caabe1f2179696dd56590df47b8df8ca
Mithil Abhiram <mithilabhiram404@gmail.com>
2025-11-27 17:02:32 +0530

    Add initial implementation of chatbot and LLM interaction
    - Created chatbot.py for user interaction with AI model.
    - Implemented chat history management and exit command.
    - Added chatmodel_gemini.py for invoking AI model with a prompt.
    - Updated requirements.txt to include necessary dependencies.
    - Added .gitignore to exclude unnecessary files and directories.

---
+++ LLMInteraction/chatmodel_gemini.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+prompt = "What is the capital City of USA?"
+
+result = model.invoke(prompt)
+
+print(result.content)
+
