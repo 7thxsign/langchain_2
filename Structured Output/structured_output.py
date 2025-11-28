49ab9fe4849ba6db80ea79b6abed9c654fb7f225
Mithil Abhiram <mithilabhiram404@gmail.com>
2025-11-28 18:41:06 +0530

    Add structured output implementation for Google Generative AI model

---
+++ Structured Output/structured_output.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
+
+#schema
+class Review(TypedDict):
+    summary: str
+    sentiment: str
+
+structured_model = model.with_structured_output(Review)
+
+prompt = '''
+This hardware is great, but the software feels kind of bloated. There are so many pre-installed apps and my phone keeps lagging when I play PUBG.
+'''
+result = structured_model.invoke(prompt)
+
+print(result)
